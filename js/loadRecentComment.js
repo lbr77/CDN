c2080783b80f0bbdc640d443a58a3b9d4c34a445
Borui Li <liborui0609@foxmail.com>
2021-08-25 17:24:04 +0800

    Update loadRecentComment.js

--- js/loadRecentComment.js
+++ js/loadRecentComment.js
@@ -4,16 +4,15 @@ function updateCommentLink(){
         pageSize:5,
         includeReply: true
 }).then(function(res){
-        console.log(res);
-        let html = ``
+        let html = ``;
         for(let i in res){
-                console.log(res[i]);
                 let domain = new URL(location.href).origin;
-                let link = domain+res[i].url+`#${res[i].id}`
+                let link = domain+res[i].url+`#${res[i].id}`;
+                let cm = res[i].commentText.slice(0,20);
                 html+=`
                 <a href="${link}" class="pull-left thumb-sm avatar m-r" style="border-radius:100%; overflow:hidden;"><img src="${res[i].avatar}"></img></a>
                 <a href="${link}" class="text-muted"><i class="iconfont icon-comments-o text-muted pull-right m-t-sm text-sm" title="详情" aria-hidden="true" data-toggle="tooltip" data-placement="auto left"></i><span class="sr-only">评论详情</span></a>
-                <div class="clear"><div class="text-ellipsis"><a href="${link}" title="${res[i].nick}"> ${res[i].nick} </a></div><small class="text-muted"><span>${res[i].commentText}</span></small></div>`
+                <div class="clear"><div class="text-ellipsis"><a href="${link}" title="${res[i].nick}"> ${res[i].nick} </a></div><small class="text-muted"><span>${cm}</span></small></div>`
                 $(".comments-list").html(html);
         }
 
